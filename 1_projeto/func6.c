1eccfb89d3a28fe0a96902cc1c5856ec2e5452fb
JvAlm71 <victoralmeida71@hotmail.com>
2023-11-18 13:53:47 -0300

    Ajustando algumas coisas, Prototipos...

--- 1_projeto/func6.c
+++ 1_projeto/func6.c
@@ -1,41 +1,50 @@
-#include "structsBTree.h"
-#include "B-Tree.h"
+#include "structs.h"
 #include "func3.h"
-#include "func4.h" // Include functionality 4
+#include "func4.h" 
+#include "func5.h" 
+#include "B-Tree.h"
 
-/*
-void functionality_6(char *dataFileName, char *indexFileName, int n) {
+void functionality_6(const char dataFileName[], const char indexFileName[], int n) {
+    // Open the binary data file and B-tree index file
     FILE *dataFile = fopen(dataFileName, "rb");
     FILE *indexFile = fopen(indexFileName, "rb");
-
     if (dataFile == NULL || indexFile == NULL) {
-        printf("Falha no processamento do arquivo.\n");
+        printf("File processing failed.\n");
+        if(dataFile) fclose(dataFile);
+        if(indexFile) fclose(indexFile);
         return;
     }
 
+    // Code to check the status of the data and index files should go here
+
     for (int i = 0; i < n; i++) {
-        char fieldName[MAX_FIELD_NAME_SIZE];
-        char fieldValue[MAX_FIELD_VALUE_SIZE];
+        char fieldName[MAX_STRING_LENGTH];
+        char fieldValue[MAX_STRING_LENGTH];
 
-        // Read the field name and value from the user
         scanf("%s", fieldName);
-        scan_quote_string(fieldValue); // Assuming function to read string with quotes
+        scan_quote_string(fieldValue); // Works for both quoted and non-quoted strings
 
+        // Check if the field is the search key
         if (strcmp(fieldName, "nomeTecnologiaOrigemDestino") == 0) {
-            // Search using B-tree index and functionality 4
-         // int RRN = searchBTree(indexFile, fieldValue); // searchBTree should return the RRN
-            if (RRN != -1) {
-                functionality_4(dataFileName, RRN);
+            // Search using B-tree
+            int recordRRN = search_btree(indexFile, fieldValue); // Assuming search_btree returns the RRN or -1 if not found
+            if(recordRRN != -1) {
+                Data recordData;
+                if (retrieve_record(dataFile, recordRRN, &recordData)) { // Assuming retrieve_record returns 1 on success
+                    print_record(&recordData); // Use the same printing function as functionality 3
+                } else {
+                    printf("Record not found.\n");
+                }
             } else {
-                printf("Registro inexistente.\n");
+                printf("Record not found.\n");
             }
         } else {
-            // Search using functionality 3
-            functionality_3(dataFileName, fieldName, fieldValue);
+            //if not...
+            // Sequential search as in functionality 3
+            // ... (Here you would use the functionality 3 code to perform the sequential search)
         }
     }
 
     fclose(dataFile);
     fclose(indexFile);
 }
-*/
